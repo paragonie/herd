8023f0aa8804cf8da0bbeaa969ff87facd8ec803
Paragon Initiative Enterprises <security@paragonie.com>
2017-12-26 13:42:45 -0500

    Implement get-update command.

--- src/CommandLine/Command/GetUpdate.php
+++ src/CommandLine/Command/GetUpdate.php
@@ -3,13 +3,16 @@ declare(strict_types=1);
 namespace ParagonIE\Herd\CommandLine\Command;
 
 use GetOpt\{
+    GetOpt,
     Operand,
     Option
 };
 use ParagonIE\Herd\CommandLine\{
     CommandInterface,
+    ConfigurableTrait,
     DatabaseTrait
 };
+use ParagonIE\Herd\Exception\EncodingError;
 
 /**
  * Class GetUpdate
@@ -17,6 +20,7 @@ use ParagonIE\Herd\CommandLine\{
  */
 class GetUpdate implements CommandInterface
 {
+    use ConfigurableTrait;
     use DatabaseTrait;
 
     /**
@@ -24,7 +28,9 @@ class GetUpdate implements CommandInterface
      */
     public function getOptions(): array
     {
-        return [];
+        return [
+            new Option('c', 'config', GetOpt::REQUIRED_ARGUMENT)
+        ];
     }
 
     /**
@@ -32,16 +38,66 @@ class GetUpdate implements CommandInterface
      */
     public function getOperands(): array
     {
-        return [];
+        return [
+            new Operand('vendor', Operand::REQUIRED),
+            new Operand('product', Operand::REQUIRED),
+            new Operand('version', Operand::REQUIRED)
+        ];
     }
 
     /**
      * @param array<int, string> $args
      * @return int
+     * @throws \Exception
      */
     public function run(...$args): int
     {
+        /**
+         * @var string $vendor
+         * @var string $product
+         * @var string $version
+         */
+        list($vendor, $product, $version) = $args;
+        $db = $this->getDatabase($this->configPath);
 
+        /** @var array<string, string> $data */
+        $data = $db->row(
+            "
+                SELECT
+                    v.name AS vendor,
+                    p.name AS product,
+                    h.contents AS history,
+                    h.hash,
+                    h.summaryhash,
+                    u.*
+                FROM
+                    herd_vendors v
+                LEFT JOIN
+                    herd_products p ON p.vendor = v.id
+                LEFT JOIN
+                    herd_product_updates u ON u.product = p.id
+                LEFT JOIN
+                    herd_history h ON u.history = h.id
+                WHERE
+                    v.name = ?
+                    AND p.name = ?
+                    AND u.version = ?
+            ",
+            $vendor,
+            $product,
+            $version
+        );
+        if (empty($data)) {
+            echo '[]', PHP_EOL;
+            exit(2);
+        }
+
+        /** @var string $encoded */
+        $encoded = \json_encode($data, JSON_PRETTY_PRINT);
+        if (!\is_string($encoded)) {
+            throw new EncodingError('Could not encode fact into a JSON string');
+        }
+        echo $encoded, PHP_EOL;
         return 0;
     }
 
@@ -49,11 +105,22 @@ class GetUpdate implements CommandInterface
      * Use the options provided by GetOpt to populate class properties
      * for this Command object.
      *
-     * @param array $args
+     * @param array<string, string> $args
      * @return self
+     * @throws \Exception
      */
     public function setOpts(array $args = [])
     {
+        if (isset($args['config'])) {
+            $this->setConfigPath($args['config']);
+        } elseif (isset($args['c'])) {
+            $this->setConfigPath($args['c']);
+        } else {
+            $this->setConfigPath(
+                \dirname(\dirname(\dirname(__DIR__))) .
+                '/data/config.json'
+            );
+        }
         return $this;
     }
 
