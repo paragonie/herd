8b573e3ae8ad9218bf9537c73ca13402dc3278a6
Paragon Initiative Enterprises <security@paragonie.com>
2017-12-26 15:31:25 -0500

    Implement vendor-keys command.

--- src/CommandLine/Command/VendorKeys.php
+++ src/CommandLine/Command/VendorKeys.php
@@ -3,13 +3,16 @@ declare(strict_types=1);
 namespace ParagonIE\Herd\CommandLine\Command;
 
 use GetOpt\{
+    GetOpt,
     Operand,
     Option
 };
 use ParagonIE\Herd\CommandLine\{
     CommandInterface,
+    ConfigurableTrait,
     DatabaseTrait
 };
+use ParagonIE\Herd\Exception\EncodingError;
 
 /**
  * Class VendorKeys
@@ -17,6 +20,7 @@ use ParagonIE\Herd\CommandLine\{
  */
 class VendorKeys implements CommandInterface
 {
+    use ConfigurableTrait;
     use DatabaseTrait;
 
     /**
@@ -24,7 +28,9 @@ class VendorKeys implements CommandInterface
      */
     public function getOptions(): array
     {
-        return [];
+        return [
+            new Option('c', 'config', GetOpt::REQUIRED_ARGUMENT)
+        ];
     }
 
     /**
@@ -32,16 +38,59 @@ class VendorKeys implements CommandInterface
      */
     public function getOperands(): array
     {
-        return [];
+        return [
+            new Operand('vendor', Operand::REQUIRED)
+        ];
     }
 
     /**
      * @param array<int, string> $args
      * @return int
+     * @throws \Exception
      */
     public function run(...$args): int
     {
+        /**
+         * @var string $vendor
+         */
+        $vendor = \array_shift($args);
+        $db = $this->getDatabase($this->configPath);
 
+        /** @var array<string, string> $data */
+        $data = $db->run(
+            "
+                SELECT
+                    v.name AS vendor,
+                    k.publickey,
+                    k.name,
+                    k.created,
+                    k.modified,
+                    h.contents AS history,
+                    h.hash,
+                    h.summaryhash
+                FROM
+                    herd_vendors v
+                LEFT JOIN
+                    herd_vendor_keys k ON k.vendor = v.id
+                LEFT JOIN
+                    herd_history h ON k.history_create = h.id
+                WHERE
+                    v.name = ?
+                    AND k.trusted
+            ",
+            $vendor
+        );
+        if (empty($data)) {
+            echo '[]', PHP_EOL;
+            exit(2);
+        }
+
+        /** @var string $encoded */
+        $encoded = \json_encode($data, JSON_PRETTY_PRINT);
+        if (!\is_string($encoded)) {
+            throw new EncodingError('Could not encode fact into a JSON string');
+        }
+        echo $encoded, PHP_EOL;
         return 0;
     }
 
@@ -49,11 +98,22 @@ class VendorKeys implements CommandInterface
      * Use the options provided by GetOpt to populate class properties
      * for this Command object.
      *
-     * @param array $args
+     * @param array<string, string> $args
      * @return self
+     * @throws \Exception
      */
     public function setOpts(array $args = [])
     {
+        if (isset($args['config'])) {
+            $this->setConfigPath($args['config']);
+        } elseif (isset($args['c'])) {
+            $this->setConfigPath($args['c']);
+        } else {
+            $this->setConfigPath(
+                \dirname(\dirname(\dirname(__DIR__))) .
+                '/data/config.json'
+            );
+        }
         return $this;
     }
 
