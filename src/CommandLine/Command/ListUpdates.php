ca5a1e3bee12f1d28a2d1bba5402776be34e4d5a
Paragon Initiative Enterprises <security@paragonie.com>
2017-12-26 15:26:15 -0500

    Implement list-updates command.

--- src/CommandLine/Command/ListUpdates.php
+++ src/CommandLine/Command/ListUpdates.php
@@ -3,13 +3,16 @@ declare(strict_types=1);
 namespace ParagonIE\Herd\CommandLine\Command;
 
 use GetOpt\{
+    GetOpt,
     Operand,
     Option
 };
 use ParagonIE\Herd\CommandLine\{
     CommandInterface,
+    ConfigurableTrait,
     DatabaseTrait
 };
+use ParagonIE\Herd\Exception\EncodingError;
 
 /**
  * Class ListUpdates
@@ -17,6 +20,7 @@ use ParagonIE\Herd\CommandLine\{
  */
 class ListUpdates implements CommandInterface
 {
+    use ConfigurableTrait;
     use DatabaseTrait;
 
     /**
@@ -24,7 +28,9 @@ class ListUpdates implements CommandInterface
      */
     public function getOptions(): array
     {
-        return [];
+        return [
+            new Option('c', 'config', GetOpt::REQUIRED_ARGUMENT)
+        ];
     }
 
     /**
@@ -32,16 +38,75 @@ class ListUpdates implements CommandInterface
      */
     public function getOperands(): array
     {
-        return [];
+        return [
+            new Operand('vendor', Operand::REQUIRED),
+            new Operand('product', Operand::REQUIRED),
+            new Operand('search', Operand::OPTIONAL)
+        ];
     }
 
     /**
      * @param array<int, string> $args
      * @return int
+     * @throws \Exception
      */
     public function run(...$args): int
     {
+        $db = $this->getDatabase($this->configPath);
+        $query = "
+                SELECT
+                    v.name AS vendor,
+                    p.name AS product,
+                    h.contents AS history,
+                    h.hash,
+                    h.summaryhash,
+                    u.*
+                FROM
+                    herd_vendors v
+                LEFT JOIN
+                    herd_products p ON p.vendor = v.id
+                LEFT JOIN
+                    herd_product_updates u ON u.product = p.id
+                LEFT JOIN
+                    herd_history h ON u.history = h.id
+                WHERE
+                    v.name = ?
+                    AND p.name = ?
+            ";
+        /**
+         * @var string $vendor
+         * @var string $product
+         */
+        if (count($args) > 2) {
+            /** @var string $search */
+            list($vendor, $product, $search) = $args;
+            /** @var array<string, string> $data */
+            $data = $db->run(
+                $query . ' AND u.version LIKE ?',
+                $vendor,
+                $product,
+                $db->escapeLikeValue($search) . '%'
+            );
+        } else {
+            list($vendor, $product) = $args;
+            /** @var array<string, string> $data */
+            $data = $db->run(
+                $query,
+                $vendor,
+                $product
+            );
+        }
+        if (empty($data)) {
+            echo '[]', PHP_EOL;
+            exit(2);
+        }
 
+        /** @var string $encoded */
+        $encoded = \json_encode($data, JSON_PRETTY_PRINT);
+        if (!\is_string($encoded)) {
+            throw new EncodingError('Could not encode fact into a JSON string');
+        }
+        echo $encoded, PHP_EOL;
         return 0;
     }
 
@@ -49,11 +114,22 @@ class ListUpdates implements CommandInterface
      * Use the options provided by GetOpt to populate class properties
      * for this Command object.
      *
-     * @param array $args
+     * @param array<string, string> $args
      * @return self
+     * @throws \Exception
      */
     public function setOpts(array $args = [])
     {
+        if (isset($args['config'])) {
+            $this->setConfigPath($args['config']);
+        } elseif (isset($args['c'])) {
+            $this->setConfigPath($args['c']);
+        } else {
+            $this->setConfigPath(
+                \dirname(\dirname(\dirname(__DIR__))) .
+                '/data/config.json'
+            );
+        }
         return $this;
     }
 
